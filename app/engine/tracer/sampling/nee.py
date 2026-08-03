70becfa512763000a7c6dc9d44edf83ac6c3dcd1
Aatti13 <aattreya.ks@gmail.com>
2026-08-03 15:45:30 +0530

    feat: added direct lighting compute script

--- app/engine/tracer/sampling/nee.py
+++ app/engine/tracer/sampling/nee.py
@@ -1,6 +1,8 @@
 import taichi as ti
 from taichi.math import vec3
 
+from bvh.traverse import compute_visibility
+from brdf import BRDF
 
 @ti.func
 def sample_emissive_triangle(v0, v1, v2, u1, u2):
@@ -44,3 +46,32 @@ def solid_angle_pdf(x: vec3, x_l: vec3, n_l: vec3, pdf_area: ti.f32, single_side
       p_light = pdf_area * dist_sq / cos_theta_l
 
   return p_light
+
+@ti.func
+def compute_direct_lighting(x: vec3, n: vec3, albedo: vec3, v0: vec3, v1: vec3, v2: vec3, l_e: vec3, u1: ti.f32, u2: ti.f32, triangles: ti.template(), num_triangles: ti.i32, single_sided: ti.i32, brdf: ti.template()): # type: ignore
+  l_dir = vec3(0.0)
+  w_l = vec3(0.0)
+  p_light = 0.0
+
+  x_l, n_l, pdf_area = sample_emissive_triangle(v0, v1, v2, u1, u2)
+
+  if pdf_area > 0.0:
+    to_light = x_l - x
+    dist_to_light = to_light.norm()
+
+    if dist_to_light > 1e-8:
+      w_l = to_light / dist_to_light
+
+      cos_theta = ti.math.dot(n, w_l)
+      if cos_theta > 0.0:
+        p_light = solid_angle_pdf(x, x_l, n_l, pdf_area, single_sided)
+
+        if p_light > 0.0:
+          V = compute_visibility(x, n, x_l, dist_to_light, triangles, num_triangles)
+
+          if V > 0.0:
+            f_r = brdf.evaluate(albedo)
+
+            l_dir = (f_r * l_e * cos_theta * V) / p_light
+
+  return l_dir, w_l, p_light
