eee27777e53a2f0f28f67d7246b836dadd12dc73
Aatti13 <aattreya.ks@gmail.com>
2026-08-03 15:57:37 +0530

    feat: added weighted nee sample method

--- app/engine/tracer/sampling/nee.py
+++ app/engine/tracer/sampling/nee.py
@@ -1,8 +1,8 @@
 import taichi as ti
 from taichi.math import vec3
 
-from bvh.traverse import compute_visibility
-from brdf import BRDF
+from tracer.bvh.traverse import compute_visibility
+
 
 @ti.func
 def sample_emissive_triangle(v0, v1, v2, u1, u2):
@@ -47,8 +47,9 @@ def solid_angle_pdf(x: vec3, x_l: vec3, n_l: vec3, pdf_area: ti.f32, single_side
 
   return p_light
 
+
 @ti.func
-def compute_direct_lighting(x: vec3, n: vec3, albedo: vec3, v0: vec3, v1: vec3, v2: vec3, l_e: vec3, u1: ti.f32, u2: ti.f32, triangles: ti.template(), num_triangles: ti.i32, single_sided: ti.i32, brdf: ti.template()): # type: ignore
+def compute_direct_lighting(x: vec3, n: vec3, albedo: vec3, v0: vec3, v1: vec3, v2: vec3, l_e: vec3, u1: ti.f32, u2: ti.f32, triangles: ti.template(), num_triangles: ti.i32, single_sided: ti.i32, brdf: ti.template()):  # type: ignore
   l_dir = vec3(0.0)
   w_l = vec3(0.0)
   p_light = 0.0
@@ -71,7 +72,25 @@ def compute_direct_lighting(x: vec3, n: vec3, albedo: vec3, v0: vec3, v1: vec3, 
 
           if V > 0.0:
             f_r = brdf.evaluate(albedo)
-
             l_dir = (f_r * l_e * cos_theta * V) / p_light
 
   return l_dir, w_l, p_light
+
+
+@ti.func
+def weighted_nee_sample(x: vec3, n: vec3, albedo: vec3, v0: vec3, v1: vec3, v2: vec3, l_e: vec3, u1: ti.f32, u2: ti.f32, triangles: ti.template(), num_triangles: ti.i32, single_sided: ti.i32, brdf: ti.template()):  # type: ignore
+  l_dir, w_l, p_light = compute_direct_lighting(x, n, albedo, v0, v1, v2, l_e, u1, u2, triangles, num_triangles, single_sided, brdf)
+
+  final_l_dir = vec3(0.0)
+
+  if p_light > 0.0:
+    cos_theta = ti.math.dot(n, w_l)
+    p_bsdf = brdf.pdf(cos_theta)
+
+    p_light_sq = p_light * p_light
+    p_bsdf_sq = p_bsdf * p_bsdf
+
+    w_light = p_light_sq / (p_light_sq + p_bsdf_sq)
+    final_l_dir = l_dir * w_light
+
+  return final_l_dir
